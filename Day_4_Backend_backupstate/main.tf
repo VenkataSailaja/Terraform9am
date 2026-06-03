1a72c277af7fa0c332ac1b2aaeb4868af7736270
VenkataSailaja <sailajaperi2000@gmail.com>
2026-06-03 21:49:55 +0530

    dev1 work

---
+++ Day_4_Backend_backupstate/main.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "Name" = "My_VPC"
+    }
+}
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "Subnet1"
+    }
+}
+resource "aws_subnet" "subnet2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "Subnet2"
+    }
+}
